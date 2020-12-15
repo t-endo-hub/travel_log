d852e1d2826498ce76f6d8369490132cfad2182f
taku <ta_msy.4.11_35@icloud.com>
2020-12-15 10:11:44 +0900

    [Add]観光地更新、削除機能追加

--- app/controllers/tourist_spots_controller.rb
+++ app/controllers/tourist_spots_controller.rb
@@ -1,4 +1,6 @@
 class TouristSpotsController < ApplicationController
+  before_action :set_tourist_spot, except: [:new, :create]
+
   def new
     @tourist_spot = TouristSpot.new
   end
@@ -16,10 +18,37 @@ class TouristSpotsController < ApplicationController
   end
 
   def show
-    @tourist_spot = TouristSpot.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @tourist_spot.update(tourist_spot_params)
+      flash[:notice] = '観光地を編集しました'
+      redirect_to root_path
+    else
+      flash[:alert] = '観光地の編集に失敗しました'
+      render root_path
+    end
+  end
+
+  def destroy
+    if @tourist_spot.destroy
+      flash[:notice] = '観光地を削除しました'
+      redirect_to root_path
+    else
+      flash[:alert] = '観光地の削除に失敗しました'
+      render root_path
+    end
   end
 
   private
+
+  def set_tourist_spot
+    @tourist_spot = TouristSpot.find(params[:id])
+  end
+
   def tourist_spot_params
     params.require(:tourist_spot).permit(
       :spot_image,
