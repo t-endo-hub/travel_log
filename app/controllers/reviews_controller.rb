b2f8f15a478705838e9a6cbf171d32d9c576ed2c
taku <ta_msy.4.11_35@icloud.com>
2020-12-15 17:07:41 +0900

    [Add]レビュー編集機能、削除機能

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, except: [:index, :new, :create]
 
   def index
     @tourist_spot_reviews = Review.where(tourist_spot_id: params[:tourist_spot_id])
@@ -21,7 +22,39 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def show
+   
+  end
+
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      flash[:notice] = "レビューを編集しました"
+      redirect_to tourist_spot_reviews_path(@review.tourist_spot)
+    else
+      flash[:alert] = "レビューの編集に失敗しました"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @review.destroy
+      flash[:notice] = "レビューを削除しました"
+      redirect_to tourist_spot_reviews_path(@review.tourist_spot)
+    else
+      flash[:alert] = "レビューの削除に失敗しました"
+      render 'edit'
+    end
+  end
+
   private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
   def review_params
     params.require(:review).permit(:review_image, :title, :body, :score)
   end
