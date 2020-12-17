ee1968b586b5404afdf25fd9f0859f7c5114cf9c
taku <ta_msy.4.11_35@icloud.com>
2020-12-17 14:05:20 +0900

    [Add]ユーザー編集機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,34 @@ class UsersController < ApplicationController
   end
 
   def edit
-    
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = 'ユーザー情報を編集しました'
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = 'ユーザー情報の編集に失敗しました'
+      render 'edit'
+    end
   end
 
   private
+  
+  def user_params
+    params.require(:user).permit(
+      :name, 
+      :age, 
+      :email, 
+      :sex, 
+      :postcode, 
+      :address_city, 
+      :address_street, 
+      :address_building, 
+      :introduction,
+      :profile_image
+    )
+  end
 
   def set_user
     @user = User.find(params[:id])
