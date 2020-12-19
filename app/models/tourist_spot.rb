3024d1cf91040fd3da3342908a1c7735b897d414
taku <ta_msy.4.11_35@icloud.com>
2020-12-19 10:20:24 +0900

    [Add]観光地画像

--- app/models/tourist_spot.rb
+++ app/models/tourist_spot.rb
@@ -11,10 +11,22 @@ class TouristSpot < ApplicationRecord
   has_many :scenes, through: :tourist_spot_scenes
 
 
+  # 住所自動入力
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  
+  # 都道府県名
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
 
+  # 都道府県名
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 
    # 「行きたい！」に追加しているかを確認
-   def favorited_by?(user)
+  def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
 
@@ -23,13 +35,19 @@ class TouristSpot < ApplicationRecord
     wents.where(user_id: user.id).exists?
   end
 
-    # ジャンル検索
-    def self.genre_search(genre_search)
-      TouristSpot.joins(:tourist_spot_genres).where("tourist_spot_genres.genre_id = #{genre_search}")
-    end
-    
-    # シーン検索
-    def self.scene_search(scene_search)
-      TouristSpot.joins(:tourist_spot_scenes).where("tourist_spot_scenes.scene_id = #{scene_search}")
-    end
+  # ジャンル検索
+  def self.genre_search(genre_search)
+    TouristSpot.joins(:tourist_spot_genres).where("tourist_spot_genres.genre_id = #{genre_search}")
+  end
+  
+  # シーン検索
+  def self.scene_search(scene_search)
+    TouristSpot.joins(:tourist_spot_scenes).where("tourist_spot_scenes.scene_id = #{scene_search}")
+  end
+
+  # 都道府県検索
+  def self.prefecture_search(prefecture_search)
+    TouristSpot.where(prefecture_code: prefecture_search)
+  end
+  
 end
