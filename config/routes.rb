ee637fdf414b64e2f3003fa0f1f91117ebce593d
taku <ta_msy.4.11_35@icloud.com>
2021-01-29 21:36:32 +0900

    [Add]routesにnamespace追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,5 @@
-Rails.application.routes.draw do
-  post '/homes/guest_sign_in', to: 'homes#new_guest'
-  get 'coupons/index'
-  get 'rooms/index'
-  get 'rooms/show'
-  resources :genres
-  resources :scenes
-  get 'user/keyword/search', to: 'users#keyword_search'
-  get 'tourist_spot/keyword/search', to: 'tourist_spots#keyword_search'
-  get 'tourist_spot/genre/search', to: 'tourist_spots#genre_search'
-  get 'tourist_spot/scene/search', to: 'tourist_spots#scene_search'
-  get 'tourist_spot/prefecture/search', to: 'tourist_spots#prefecture_search'
-  get 'tourist_spot/tag/search', to: 'tourist_spots#tag_search'
-  post 'follow/:id', to: 'relationships#follow', as: 'follow'
-  post 'unfollow/:id', to: 'relationships#unfollow', as: 'unfollow'
-  get 'users/following/:user_id', to: 'users#following', as:'following'
-  get 'users/follower/:user_id', to: 'users#follower', as:'follower'
-  get 'get_genre/new', to: 'homes#new', defaults: { format: 'json' }
-  get 'favorites', to: 'tourist_spots#favorites'
-  get 'wents', to: 'tourist_spots#wents'
-
-  
-
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: "users/sessions",
-  }
-  
-  resources :users
-  resources :messages, only: [:create, :destroy]
-  resources :rooms, only: [:create, :show, :index]
-  resources :coupons, only: [:create, :index]
-  resources :notifications, only: [:index, :destroy]
-
-
-  resources :tourist_spots do
+namespace :user do
+  resources :tourist_spots, only: [:new, :create, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resource :wents, only: [:create, :destroy]
     resources :reviews do
@@ -41,11 +7,30 @@ Rails.application.routes.draw do
       resources :comments, only: [:create, :edit, :update, :destroy]
     end
     get 'map', to: 'tourist_spots#map'
+    get 'images', to: 'tourist_spots#images'
     put :sort
   end
-  root 'homes#top'
+
+  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :messages, only: [:create, :destroy]
+  resources :rooms, only: [:create, :show, :index]
+  resources :notifications, only: [:index, :destroy]
+  resources :coupons, only: [:create, :index]
+
+  get 'user/keyword/search', to: 'users#keyword_search'
+  post 'follow/:id', to: 'relationships#follow', as: 'follow'
+  post 'unfollow/:id', to: 'relationships#unfollow', as: 'unfollow'
+  get 'users/following/:user_id', to: 'users#following', as:'following'
+  get 'users/follower/:user_id', to: 'users#follower', as:'follower'
+
+  get 'favorites', to: 'tourist_spots#favorites'
+  get 'wents', to: 'tourist_spots#wents'
+  get 'tourist_spot/keyword/search', to: 'tourist_spots#keyword_search'
+  get 'tourist_spot/genre/search', to: 'tourist_spots#genre_search'
+  get 'tourist_spot/scene/search', to: 'tourist_spots#scene_search'
+  get 'tourist_spot/prefecture/search', to: 'tourist_spots#prefecture_search'
+  get 'tourist_spot/tag/search', to: 'tourist_spots#tag_search'
 
   get 'get_genre/children', to: 'tourist_spots#get_genre_children', defaults: { format: 'json' }
   get 'get_genre/grandchildren', to: 'tourist_spots#get_genre_grandchildren', defaults: { format: 'json' }
-
 end
